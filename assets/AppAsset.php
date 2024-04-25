554757c8a5c31de038a1d2532e62574811d78dfc
Yoky Ben Stevanus <juarakeduasabar@gmail.com>
2024-04-25 23:57:28 +0700

    first upload

---
+++ assets/AppAsset.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        // 'themes/default/css/site.css',
+    ];
+    public $js = [
+        [
+            'themes/skote/assets/libs/jquery/jquery.min.js', 
+            'position' => \yii\web\View::POS_HEAD
+        ],
+        'themes/skote/assets/libs/bootstrap/js/bootstrap.bundle.min.js',
+        'themes/skote/assets/libs/metismenu/metisMenu.min.js',
+        'themes/skote/assets/libs/simplebar/simplebar.min.js',
+        'themes/skote/assets/libs/node-waves/waves.min.js',
+
+        'themes/skote/assets/js/app.js',
+    ];
+    public $depends = [
+        // 'yii\web\YiiAsset',
+        // 'yii\bootstrap5\BootstrapAsset'
+    ];
+}
---
+++ config/params.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'code-id' => 'project-01',
+    'name' => 'NamaAplikasi',
+    'companyName' => 'Juara Kedua',
+    'language' => 'en',
+    'yearCopyright' => '2024',
+    'timeZone' => 'Asia/Jakarta',
+    'rightBottomText' => 'Design & Development by Juara Kedua',
+    'robotTxt' => 'noindex, nofollow',
+    'supportEmail' => 'support@example.com',
+
+
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+];
